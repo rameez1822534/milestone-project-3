6e87dbe8e487171de446b6cf8bfa070bac62eaa9
rameez1822534 <ch_rameez89@hotmail.com>
2020-08-03 01:38:44 +0200

    reviews.html added

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId 
 
 app = Flask(__name__)
+app.config["MONGO_DBNAME"] = "tech-data"
+app.config["MONGO_URI"] = "mongodb+srv://rameez1:XCoDWa4q3nYSVrwp@myfirstcluster-cbmmr.mongodb.net/tech-data?retryWrites=true&w=majority"
+mongo = PyMongo(app)
 
 
 @app.route('/')
-def hello():
-    return 'Hello World ...again'
+@app.route('/get_reviews')
+def get_reviews():
+    return render_template("reviews.html", reviews=mongo.db.reviews.find())
 
 
 if __name__ == '__main__':
